b40b0d3349d82b5134bda7ae45ba9a2e9ce64cf7
Makoo <141389899+makuu12@users.noreply.github.com>
2024-12-24 21:24:20 +0800

    appstart

---
+++ WEEK10_ANDAYAA_SBIT3J/WEEK10_ANDAYAA_SBIT3J/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WEEK10_ANDAYAA_SBIT3J
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}

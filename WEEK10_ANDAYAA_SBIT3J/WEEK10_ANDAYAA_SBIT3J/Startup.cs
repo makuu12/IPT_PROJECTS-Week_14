0ce4bbeb47089e1f69f438c494f97ee12d844581
Makoo <141389899+makuu12@users.noreply.github.com>
2025-01-17 18:15:34 +0800

    startup

---
+++ WEEK10_ANDAYAA_SBIT3J/WEEK10_ANDAYAA_SBIT3J/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WEEK10_ANDAYAA_SBIT3J.Startup))]
+namespace WEEK10_ANDAYAA_SBIT3J
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
